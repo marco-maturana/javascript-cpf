5d71984db05d6d22433bdb20959f62e66a6db3c2
Marco Aurelio Maturana Silveira <ti026@smig.com>
2018-09-25 17:53:01 -0400

    module implementation

---
+++ index.js
@@ -0,0 +1,76 @@
+// Blacklist common values.
+const BLACKLIST = [
+  '00000000000',
+  '11111111111',
+  '22222222222',
+  '33333333333',
+  '44444444444',
+  '55555555555',
+  '66666666666',
+  '77777777777',
+  '88888888888',
+  '99999999999',
+  '12345678909'
+]
+
+const STRICT_STRIP_REGEX = /[.-]/g
+const LOOSE_STRIP_REGEX = /[^\d]/g
+
+exports.verifierDigit = function (numbers) {
+  numbers = numbers
+    .split('')
+    .map(function (number) { return parseInt(number, 10) })
+
+  const modulus = numbers.length + 1
+
+  const multiplied = numbers.map(function (number, index) {
+    return number * (modulus - index)
+  })
+
+  const mod = multiplied.reduce(function (buffer, number) {
+    return buffer + number
+  }) % 11
+
+  return (mod < 2 ? 0 : 11 - mod)
+}
+
+exports.format = function (number) {
+  return this.strip(number).replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4')
+}
+
+exports.strip = function (number, strict) {
+  const regex = strict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX
+  return (number || '').toString().replace(regex, '')
+}
+
+exports.isValid = function (number, strict) {
+  const stripped = this.strip(number, strict)
+
+  // CPF must be defined
+  if (!stripped) { return false }
+
+  // CPF must have 11 chars
+  if (stripped.length !== 11) { return false }
+
+  // CPF can't be blacklisted
+  if (BLACKLIST.includes(stripped)) { return false }
+
+  let numbers = stripped.substr(0, 9)
+  numbers += this.verifierDigit(numbers)
+  numbers += this.verifierDigit(numbers)
+
+  return numbers.substr(-2) === stripped.substr(-2)
+}
+
+exports.generate = function (formatted) {
+  let numbers = ''
+
+  for (let i = 0; i < 9; i += 1) {
+    numbers += Math.floor(Math.random() * 9)
+  }
+
+  numbers += this.verifierDigit(numbers)
+  numbers += this.verifierDigit(numbers)
+
+  return (formatted ? this.format(numbers) : numbers)
+}
